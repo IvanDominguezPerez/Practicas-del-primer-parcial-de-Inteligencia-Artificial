9ecd952bbf203295f8e218fb92cb5a9b539e3505
IvanDominguezPerez <160158111+IvanDominguezPerez@users.noreply.github.com>
2024-03-01 09:11:09 -0600

    Práctica 1
    Ejercicios de Variables

---
+++ 001_Variables.py
@@ -0,0 +1,11 @@
+#Practica: 001_Variables
+#Alumno: Ivan Dominguez
+#Registro: 21310234
+#Grupo: 6E1
+
+Ivariable = "Esta es la primera practica" #Creamos una variable y almacenamos un string
+Inum1 = 11 #Creamos una variable y almacenamos un dato numerico
+Inum2 = 20 #Creamos una sgunda variable y almacenamos otro dato numerico
+Inum3 = Inum1 + Inum2 #Creamos una tercera variable y almacenamos en ella la suma de las dos variables anteriores
+print(Inum3) #Imprimimos el valor de la suma, o sea la tercera variable
+
