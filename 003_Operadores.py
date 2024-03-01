7d860bed6ff52a37e2bdc508aba697a6b8e08a04
IvanDominguezPerez <160158111+IvanDominguezPerez@users.noreply.github.com>
2024-03-01 10:12:51 -0600

    Práctica 3
    Un conjunto de códigos que abarcan el uso de los operadores: -Suma, resta, multiplicación, división -Exponentes -Números flotantes y redondeos

---
+++ 003_Operadores.py
@@ -0,0 +1,39 @@
+#003_Operadores
+
+#Practica: 006_Operadores_Suma_Resta_Division_Multiplicacion
+#Alumno: Ivan Dominguez
+#Registro: 21310234
+#Grupo: 6E1
+
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
+
+#Las cuantro lineas de arriba tienen la función de poder colocar acentos
+
+Isuma = 20 + 23 + 44 #Creamos la variable suma y sumamos numeros para obtener 87 de resultado usando 20 y 23
+print("Suma: ",Isuma) #Imprimimos la variable suma
+Iresta = 20 + 23 - 130 #Creamos la variable resta y restamos numeros para obtener -87 de resultado usando 20 y 23
+print("Resta: ",Iresta) #Imprimimos la variable resta
+Imulti = 20 * 23 * 1.8914 #Creamos la variable multi y multiplicamos numeros para obtener 870 de resultado usando 20 y 23
+print("Multiplicación: ",Imulti) #Imprimimos la variable multi
+Idivi = 5000 / 230 / 2 #Creamos la variable divi y dividimos numeros para obtener 10 de resultado usando 5000 y 230
+print("División: ",Idivi) #Imprimimos la variable divi
+operacion = 10 / 5 + 15 - 17 #Creamos la variable operación y con sibolos básicos ordenados el resultado debe ser 0 con los numeros dados
+print("Operación: ",operacion) #Imprimimos la variable operación
+
+#Practica: 007_Operadores_Exponentes
+#Alumno: Ivan Dominguez
+#Registro: 21310234
+#Grupo: 6E1
+
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
+
+#Las cuantro lineas de arriba tienen la función de poder colocar acentos
+
+Ivar = 2*2*2*2*2*2*2*2*2*2 #para conseguir 1024 con el numero dos es necesario multiplicarlo 10 veces
+print("El resultado de elevar 2 a la 10 es de: ",Ivar) #imprimimos el valor de la variable Ivar
